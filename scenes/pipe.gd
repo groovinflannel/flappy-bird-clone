e4394afc45a4781a70a7c88ff270b5622371ef31
groovinflannel <groovinflannel@gmail.com>
2026-06-23 23:08:51 +0100

    changed Ground scene to use Parallax2D node, add code to player.gd for constant right movement

--- scenes/pipe.gd
+++ scenes/pipe.gd
@@ -4,7 +4,7 @@ extends AnimatableBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pipe.position = Vector2(1200, 400)
+	pipe.position = Vector2(1200, 500)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 3.0
+const MAX_SPEED = 5.0
 const JUMP_VELOCITY = -400.0
 
+func _ready() -> void:
+	velocity.x = SPEED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,12 +17,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and !is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Handle constant rightward movement
+	velocity.x += SPEED
+	if velocity.x == MAX_SPEED:
+		velocity.x = SPEED
+		velocity.x += SPEED
 
 	move_and_slide()
