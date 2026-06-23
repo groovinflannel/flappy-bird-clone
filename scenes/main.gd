728774a0b2b35d6a32b41749297a7f6a978a0510
groovinflannel <groovinflannel@gmail.com>
2026-06-23 11:12:58 +0100

    Instantiated pipe in main scene, created ground.tscn, imported ground sprite/tileset

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+var pipe = preload("res://scenes/pipe.tscn")
+var pipe_instance = pipe.instantiate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	add_child(pipe_instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/pipe.gd
+++ scenes/pipe.gd
@@ -1,9 +1,10 @@
 extends AnimatableBody2D
 
+@onready var pipe: AnimatableBody2D = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pipe.position = Vector2(1200, 400)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
