c1249fc775010a38961ae651b5a157e1fda11902
groovinflannel <groovinflannel@gmail.com>
2026-06-25 10:17:52 +0100

    added idle animation, set gravity to 0 before button is pressed, added additional left mouse click input for flapping

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,27 +2,32 @@ extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-const SPEED = 3.0
+var speed = 0.0
 const MAX_SPEED = 5.0
 const JUMP_VELOCITY = -400.0
+var gravity = Vector2(0.0, 0.0)
+
 
 func _ready() -> void:
-	velocity.x = SPEED
+	animation_player.play("idle")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += gravity * delta
+		
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and !is_on_floor():
+	if Input.is_action_just_pressed("flap") and !is_on_floor():
+		gravity = Vector2(0.0, 980.0)
+		speed = 3.0
 		velocity.y = JUMP_VELOCITY
 		animation_player.play("flapping")
 
 	# Handle constant rightward movement
-	velocity.x += SPEED
+	velocity.x += speed
 	if velocity.x == MAX_SPEED:
-		velocity.x = SPEED
-		velocity.x += SPEED
+		velocity.x = speed
+		velocity.x += speed
 
 	move_and_slide()
