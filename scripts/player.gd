00e1e4ecbd328db476eb4c57ba667e034b1745f0
groovinflannel <groovinflannel@gmail.com>
2026-06-26 11:39:33 +0100

    restructured ground scene to be static body 2d

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var speed = 0.0
+#var speed = 0.0
 const MAX_SPEED = 5.0
 const JUMP_VELOCITY = -400.0
 var gravity = Vector2(0.0, 0.0)
 
 
 func _ready() -> void:
-	animation_player.play("idle")
+	idle()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -20,14 +20,22 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("flap") and !is_on_floor():
 		gravity = Vector2(0.0, 980.0)
-		speed = 3.0
+		#speed = 3.0
 		velocity.y = JUMP_VELOCITY
-		animation_player.play("flapping")
+		flapping()
 
 	# Handle constant rightward movement
-	velocity.x += speed
-	if velocity.x == MAX_SPEED:
-		velocity.x = speed
-		velocity.x += speed
+	#velocity.x += speed
+	#if velocity.x == MAX_SPEED:
+		#velocity.x = speed
+		#velocity.x += speed
 
 	move_and_slide()
+
+
+func idle() -> void:
+	animation_player.play("idle")
+
+
+func flapping() -> void:
+	animation_player.play("flapping")
